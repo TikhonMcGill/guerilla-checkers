c809bd77a9bebc305be8dc2f437d4e121dae25d8
Tikhon McGill <tm2008@hw.ac.uk>
2024-01-29 13:32:32 +0000

    Remove temporary test code

--- assets/scenes/game_scene/game_scene.gd
+++ assets/scenes/game_scene/game_scene.gd
@@ -18,25 +18,3 @@ func _unhandled_key_input(event):
 
 func _on_quit_confirmation_dialog_confirmed():
 	get_tree().change_scene_to_file(MAIN_MENU_PATH)
-
-func _on_game_board_mouse_over_tile(tile : Tile):
-	var tile_index : int = game_board.cells.find(tile)
-	
-	if tile_index == -1:
-		return
-	
-	tile.color = tile.color.inverted()
-	
-	for x in game_state.get_cell_corners(tile_index):
-		var corner : Corner = game_board.corners.get_child(x)
-		corner.color = corner.color.inverted()
-
-func _on_game_board_mouse_over_corner(corner : Corner):
-	var corner_index : int = corner.get_index()
-	
-	corner.color = corner.color.inverted()
-	
-	for x in game_state.get_adjacent_corners(corner_index):
-		print(x)
-		var corner2 : Corner = game_board.corners.get_child(x)
-		corner2.color = corner2.color.inverted()
